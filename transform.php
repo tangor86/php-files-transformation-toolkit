f3ed95bfc9416d58fb8d8d170b3e144b811836a4
Jevgenijs <tangor@cagoi.com>
2021-10-21 23:58:25 +0300

    211021

--- transform.php
+++ transform.php
@@ -1,44 +1,81 @@
 <?php
 
 	define("SEP", "\\");
+	define("DEB", 1);
 
 	//settings class!
 	class transformer {
+
+		static $jsonSettingsFileName = 'php-transform.json';
+
 		static $rules = array(
 			'workingDir' => 'C:\Users\EUGENY\vvv-local\www\sample-theme\public_html',
-			'targetDir' => 'C:\Users\EUGENY\vvv-local\www\wordpress-one\public_html\wp-content\themes\gogogo',
-			'items' => [
-				array(
-					'command' => 'copyFile',
-					'filename' => 'style.css'
-				)
-			]
+			'targetDir' => 'C:\Users\EUGENY\vvv-local\www\wordpress-one\public_html\wp-content\themes\gogogo'
 		);
 	}
 	
-	function getTargetPath($fName) {
-		//return $transformationObj['targetDir'] . SEP . $fName;
-		return transformer::$rules['targetDir'] . SEP . $fName;
-	}
-
 	/**
-	 * Command to copy a file!
-	 */
-	function cmd_copyFile($item) {
-		
-		$myfile = fopen(getTargetPath($item['filename']), "w") or die("Unable to open file!");
-		//$myfile = fopen("style2.css", "w") or die("Unable to open file!");
-		$txt = file_get_contents(transformer::$rules['workingDir'] . SEP . $item['filename']);
-		fwrite($myfile, $txt);
-		fclose($myfile);
+	 * Parent class for all actions!
+	*/
+	class action {
 
-		return true;
+	    function __construct() {
+	        if (DEB) print "In constructor " . get_class($this) . "\n";
+	    }
+
+	    function __destruct() {
+			if (DEB) print "Destroying " . get_class($this) . "\n";
+	    }
+
+		function getTargetPath($fName) {
+			//return $transformationObj['targetDir'] . SEP . $fName;
+			return transformer::$rules['targetDir'] . SEP . $fName;
+		}
+
+	    function perform($item) {
+	    	//should be redefined at all childs!
+	    }
+	}
+
+	//CopyFile class!
+	class actionCopyFile extends action {
+
+		function perform($item) {
+			$myfile = fopen($this->getTargetPath($item['fileName']), "w") or die("Unable to open file!");
+			//$myfile = fopen("style2.css", "w") or die("Unable to open file!");
+			$txt = file_get_contents(transformer::$rules['workingDir'] . SEP . $item['fileName']);
+			fwrite($myfile, $txt);
+			fclose($myfile);
+
+			return true;
+		}
+	}
+
+	
+	//var_dump($argv);
+	transformer::$rules['workingDir'] = $argv[1];
+
+
+	$string = file_get_contents($argv[1] . SEP . transformer::$jsonSettingsFileName);
+	$rulesJson = json_decode($string, true);
+
+	echo "Running transform.php at " . date("h:i:sa") . " for " . count($rulesJson['actions']) . " items!\n";
+
+	if (DEB) {
+		echo "Passed rules:\n";
+		print_r($rulesJson);
+	}
+
+	foreach ($rulesJson['actions'] as $v) {
+		$clName = 'action' . $v['action'];
+		$clObj = new $clName();
+		call_user_func(array($clObj, 'perform'), $v);
+
+		//call_user_func(array('action'.$v['action'], 'perform'), $v);
 	}
 
 
-	echo "Running transform.php at " . date("h:i:sa") . " for " . count(transformer::$rules['items']) . " items!\n";
-	echo "Executing...\n";
-
+	/*
 	foreach (transformer::$rules['items'] as $v) {
 
 		$ret = false;
@@ -56,5 +93,5 @@
 
 		echo " -> " . ($ret?'done':'failed!') . "\n";
 	}
-
+	*/
 ?>
