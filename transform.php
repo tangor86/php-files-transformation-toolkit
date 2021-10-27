6c2365f5275c6e21536da01eb2c656194e52289d
Jevgenijs <tangor@cagoi.com>
2021-10-27 21:19:47 +0300

    271021

--- transform.php
+++ transform.php
@@ -3,7 +3,8 @@
 	define("SEP", "\\");		// dir separator
 	define("DEB", 1); 			// 1 - basic, 2 - moderate, 3 - detailed
 
-	include "transformClasses.php";
+	include "classTransformationOrchestrator.php";
+	include "classAction.php";
 	
 	//init
 	//Example: "php C:\Users\EUGENY\vvv-local\www\php-files-transformation-toolkit\transform.php c:\Users\EUGENY\vvv-local\www\sample-theme\public_html"
