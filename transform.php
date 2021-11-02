70292ef6b388e29c7367eb2cb8c25ee5aa1ae13b
Jevgenijs <tangor@cagoi.com>
2021-11-02 21:08:33 +0200

    011121

--- transform.php
+++ transform.php
@@ -1,8 +1,9 @@
 <?php
 
-	define("SEP", "\\");		// dir separator
-	define("DEB", 0); 			// 1 - basic, 2 - moderate, 3 - detailed
-
+	define("SEP", "\\");				// dir separator
+	define("CLASS_DIR", "classes");		// directory which contains class files
+	define("DEB", 0); 					// 1 - basic, 2 - moderate, 3 - detailed
+	
 	function d($msg, $lvl = 1) {
 		if ($lvl <= DEB) {
 			if (gettype($msg) == "array") {
@@ -12,6 +13,23 @@
 			}
 		}
 	}
+
+	spl_autoload_register(function (string $className): void {
+
+		//require "{$_SERVER['DOCUMENT_ROOT']}\{$className}.php";
+		
+		if (class_exists($className)) {
+            return;
+        }
+
+		if (file_exists($className)) {
+			require_once("./{$className}.php");
+		} else {
+			require_once(CLASS_DIR . SEP . "{$className}.php");
+		}
+
+	});
+
 
 	/*
 	function log_error( $num, $str, $file, $line, $context = null )
@@ -27,12 +45,9 @@
 	set_error_handler( "log_error" );
 	set_exception_handler( "log_exception" );
 	*/
-
-
-	include "classTransformationOrchestrator.php";
-	include "classAction.php";
-	include "classOutputStats.php";
 	
+	require_once(CLASS_DIR . SEP . 'action.php');
+
 	//init
 	//Example: "php C:\Users\EUGENY\vvv-local\www\php-files-transformation-toolkit\transform.php c:\Users\EUGENY\vvv-local\www\sample-theme\public_html"
 	$mainClass = new transformationOrchestrator();
